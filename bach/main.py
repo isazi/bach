108f398ce651d4735cc5fe49b2b1fbe7408fee2f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-14 11:35:02 +0800

    Added second demo that works on a recorded video.

--- bach/main.py
+++ bach/main.py
@@ -12,30 +12,38 @@ def command_line():
     parser.add_argument("-c", "--config_path", help="File containing darknet configuration", type=str, required=True)
     parser.add_argument("-m", "--meta_path", help="File containing darknet metadata", type=str, required=True)
     parser.add_argument("-w", "--weights_path", help="File containing darknet weights", type=str, required=True)
-    # Demo
-    parser_demo = subparsers.add_parser("demo")
+    # Demo webcam
+    parser_demo = subparsers.add_parser("demo-webcam")
     parser_demo.add_argument("--webcam", help="The ID of the webcam", type=int, required=True)
     parser_demo.add_argument("--width", help="Webcam's resolution width", type=int, default=640)
     parser_demo.add_argument("--height", help="Webcam's resolution height", type=int, default=480)
     parser_demo.add_argument("--threshold", help="Detection threshold", type=float, default=0.5)
     parser_demo.add_argument("--gray", help="Convert input to grayscale", action="store_true")
+    # Demo file
+    parser_demo = subparsers.add_parser("demo-file")
+    parser_demo.add_argument("--file", help="The file containing the video", type=str, required=True)
+    parser_demo.add_argument("--threshold", help="Detection threshold", type=float, default=0.5)
+    parser_demo.add_argument("--gray", help="Convert input to grayscale", action="store_true")
     return parser.parse_args()
 
 
-def demo(arguments):
+def demo(arguments, read_file=False):
     detector = bach.detector.Detector(arguments.config_path, arguments.meta_path, arguments.weights_path)
     code = detector.initialize()
     if not code:
         print("Impossible to initialize darknet.")
         exit(-1)
-    webcam = bach.video.Webcam(webcam_id=arguments.webcam, width=arguments.width, height=arguments.height)
-    webcam.initialize()
-    if not webcam.ready():
-        print("Impossible to initialize webcam.")
+    if read_file:
+        video = bach.video.VideoFile(arguments.file)
+    else:
+        video = bach.video.Webcam(webcam_id=arguments.webcam, width=arguments.width, height=arguments.height)
+    video.initialize()
+    if not video.ready():
+        print("Impossible to open video source.")
         exit(-1)
     while True:
         try:
-            frame = webcam.get_frame(gray=arguments.gray)
+            frame = video.get_frame(gray=arguments.gray)
         except ValueError as err:
             print("Error: ".format(str(err)))
             exit(-1)
