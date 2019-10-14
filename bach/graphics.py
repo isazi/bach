a27e115465c6235d7e24ebc6897571271078d896
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-14 10:41:09 +0800

    Move the label outside of the box.

--- bach/graphics.py
+++ bach/graphics.py
@@ -5,6 +5,9 @@ def draw_bounding_box(img, label, color, x, y, w, h):
     """
     Draw a bounding box around an object.
     """
-    cv2.rectangle(img, (int(x - (w / 2)), int(y - (h / 2))), (int(x + (w / 2)), int(y + (h / 2))), color, 2)
-    cv2.putText(img, label, (int(x - 10), int(y - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    top_left = (int(x - (w / 2)), int(y - (h / 2)))
+    bottom_right = (int(x + (w / 2)), int(y + (h / 2)))
+    cv2.rectangle(img, top_left, bottom_right, color, 2)
+    label_point = (int(x - (w / 2)), int(y - (h / 2)) - 10)
+    cv2.putText(img, label, label_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
     return img
