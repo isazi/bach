eaa28b560529ee05e9245356726f2ec13f06c4be
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-14 12:47:31 +0800

    Save a video with the detections.

--- bach/main.py
+++ bach/main.py
@@ -12,22 +12,24 @@ def command_line():
     parser.add_argument("-c", "--config_path", help="File containing darknet configuration", type=str, required=True)
     parser.add_argument("-m", "--meta_path", help="File containing darknet metadata", type=str, required=True)
     parser.add_argument("-w", "--weights_path", help="File containing darknet weights", type=str, required=True)
-    # Demo webcam
-    parser_demo = subparsers.add_parser("demo-webcam")
-    parser_demo.add_argument("--webcam", help="The ID of the webcam", type=int, required=True)
-    parser_demo.add_argument("--width", help="Webcam's resolution width", type=int, default=640)
-    parser_demo.add_argument("--height", help="Webcam's resolution height", type=int, default=480)
-    parser_demo.add_argument("--threshold", help="Detection threshold", type=float, default=0.5)
-    parser_demo.add_argument("--gray", help="Convert input to grayscale", action="store_true")
-    # Demo file
-    parser_demo = subparsers.add_parser("demo-file")
-    parser_demo.add_argument("--file", help="The file containing the video", type=str, required=True)
-    parser_demo.add_argument("--threshold", help="Detection threshold", type=float, default=0.5)
-    parser_demo.add_argument("--gray", help="Convert input to grayscale", action="store_true")
+    # Live webcam detection
+    parser_lwd = subparsers.add_parser("live_webcam_detection")
+    parser_lwd.add_argument("--webcam", help="The ID of the webcam", type=int, required=True)
+    parser_lwd.add_argument("--width", help="Webcam's resolution width", type=int, default=640)
+    parser_lwd.add_argument("--height", help="Webcam's resolution height", type=int, default=480)
+    parser_lwd.add_argument("--threshold", help="Detection threshold", type=float, default=0.5)
+    parser_lwd.add_argument("--gray", help="Convert input to grayscale", action="store_true")
+    parser_lwd.add_argument("--output", help="File where to save the output video", type=str)
+    # Recorded video detection
+    parser_rvd = subparsers.add_parser("recorded_video_detection")
+    parser_rvd.add_argument("--file", help="The file containing the video", type=str, required=True)
+    parser_rvd.add_argument("--threshold", help="Detection threshold", type=float, default=0.5)
+    parser_rvd.add_argument("--gray", help="Convert input to grayscale", action="store_true")
+    parser_rvd.add_argument("--output", help="File where to save the output video", type=str)
     return parser.parse_args()
 
 
-def demo(arguments, read_file=False):
+def video_detection(arguments, read_file=False):
     detector = bach.detector.Detector(arguments.config_path, arguments.meta_path, arguments.weights_path)
     code = detector.initialize()
     if not code:
@@ -38,6 +40,9 @@ def demo(arguments, read_file=False):
     else:
         video = bach.video.Webcam(webcam_id=arguments.webcam, width=arguments.width, height=arguments.height)
     video.initialize()
+    if arguments.output:
+        output = bach.video.VideoWriter(arguments.output, width=arguments.width, height=arguments.height)
+        output.initialize()
     if not video.ready():
         print("Impossible to open video source.")
         exit(-1)
@@ -55,6 +60,7 @@ def demo(arguments, read_file=False):
                                             0,
                                             detection[2][0], detection[2][1], detection[2][2], detection[2][3])
         cv2.imshow('frame', frame)
+        output.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.destroyAllWindows()
@@ -63,10 +69,10 @@ def demo(arguments, read_file=False):
 
 def __main__():
     arguments = command_line()
-    if arguments.action == "demo-webcam":
-        demo(arguments)
-    elif arguments.action == "demo-file":
-        demo(arguments, read_file=True)
+    if arguments.action == "live_webcam_detection":
+        video_detection(arguments)
+    elif arguments.action == "recorded_video_detection":
+        video_detection(arguments, read_file=True)
 
 
 __main__()
