6f2d4eafb08a647b3d825d29fd86dc04e3200ba4
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-18 20:20:34 +0800

    Add a sharpen image function.

--- bach/graphics.py
+++ bach/graphics.py
@@ -11,3 +11,12 @@ def draw_bounding_box(img, label, color, x, y, w, h):
     label_point = (int(x - (w / 2)), int(y - (h / 2)) - 10)
     cv2.putText(img, label, label_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
     return img
+
+
+def sharpen_image(image):
+    """
+    Return a sharpened version of an image.
+    """
+    blur = cv2.GaussianBlur(image, (0, 0), 3)
+    sharp = cv2.addWeighted(image, 1.5, blur, -0.5, 0)
+    return sharp
