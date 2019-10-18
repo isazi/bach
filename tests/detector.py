a4a6d2963818ba97b2a3163c4a7dbf2f58990ca2
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-18 10:00:36 +0800

    Cleaner test.

--- tests/detector.py
+++ tests/detector.py
@@ -6,31 +6,31 @@ import bach.graphics
 WEBCAM_ID = 0
 
 
-detector = bach.detector.Detector("/home/alessio/Downloads/2019-10-10/yolo-ants.cfg",
-                                  "/home/alessio/Downloads/2019-10-10/yolo-ants.data",
-                                  "/home/alessio/Downloads/2019-10-10/yolo-ants_final.weights")
+detector = bach.detector.Detector("yolo.cfg",
+                                  "yolo.data",
+                                  "yolo.weights")
 code = detector.initialize()
 if not code:
     print("Impossible to initialize darknet.")
     exit(-1)
+
 webcam = bach.video.Webcam(webcam_id=WEBCAM_ID, width=1920, height=1080)
 webcam.initialize()
-if not webcam.ready():
-    exit(-1)
-while True:
+
+while webcam.ready():
     try:
         frame = webcam.get_frame()
     except ValueError as err:
         print("Error: ".format(str(err)))
-        exit(-1)
+        break
     processed_frame = detector.preprocess_frame(frame)
-    detections = detector.process_frame(processed_frame, threshold=0.01)
+    detections = detector.process_frame(processed_frame, threshold=0.25)
     for detection in detections:
-        bach.graphics.draw_bounding_box(frame,
+        bach.graphics.draw_bounding_box(processed_frame,
                                         detection[0],
                                         detector.colors[detection[0]],
                                         detection[2][0], detection[2][1], detection[2][2], detection[2][3])
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', processed_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
