c755f65b60505299c3141c3044e7b1459bf6c6d7
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-21 16:17:21 +0800

    Provide coordinates of top left and bottom right corners.

--- bach/objects.py
+++ bach/objects.py
@@ -8,3 +8,15 @@ class Entity:
         self.width = width
         self.height = height
         self.position = None
+
+    def top_left(self):
+        """
+        Coordinates of the top left corner of the entity.
+        """
+        return {int(self.position.x - (self.width / 2)), int(self.position.y - (self.height / 2))}
+
+    def bottom_right(self):
+        """
+        Coordinates of the bottom right corner of the entity.
+        """
+        return {int(self.position.x + (self.width / 2)), int(self.position.y + (self.height / 2))}
