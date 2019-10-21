da784c0eb9a19db164b70a1f1777afeccf0bd9ae
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-21 16:20:40 +0800

    Fix bug.

--- bach/objects.py
+++ bach/objects.py
@@ -13,10 +13,10 @@ class Entity:
         """
         Coordinates of the top left corner of the entity.
         """
-        return {int(self.position.x - (self.width / 2)), int(self.position.y - (self.height / 2))}
+        return int(self.position.x - (self.width / 2)), int(self.position.y - (self.height / 2))
 
     def bottom_right(self):
         """
         Coordinates of the bottom right corner of the entity.
         """
-        return {int(self.position.x + (self.width / 2)), int(self.position.y + (self.height / 2))}
+        return int(self.position.x + (self.width / 2)), int(self.position.y + (self.height / 2))
