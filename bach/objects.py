86247fdf7823818c7ce6f2bbd0f538d8e643871a
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-21 15:28:53 +0800

    Added width and height.

--- bach/objects.py
+++ bach/objects.py
@@ -1,8 +1,10 @@
 
-class Object:
-    def __init__(self, label=""):
+class Entity:
+    def __init__(self, label="", width=1, height=1):
         """
         Default constructor.
         """
         self.label = label
+        self.width = width
+        self.height = height
         self.position = None
