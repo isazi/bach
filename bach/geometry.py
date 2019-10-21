5842e55762f94594f2adc71a033253d40657884c
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-21 16:27:24 +0800

    Check if point is inside the entity.

--- bach/geometry.py
+++ bach/geometry.py
@@ -6,13 +6,3 @@ class Point:
         """
         self.x = x
         self.y = y
-
-
-def inside_box(point, box_upper_left, w, h):
-    """
-    Check if a point is inside a box.
-    """
-    if ((point.x > box_upper_left.x) and (point.x < box_upper_left.x + w)) \
-            and ((point.y > box_upper_left.y) and (point.y < box_upper_left.y + h)):
-        return True
-    return False
--- bach/objects.py
+++ bach/objects.py
@@ -20,3 +20,14 @@ class Entity:
         Coordinates of the bottom right corner of the entity.
         """
         return int(self.position.x + (self.width / 2)), int(self.position.y + (self.height / 2))
+
+    def is_inside(self, point):
+        """
+        Check if a point is inside the entity.
+        """
+        top_left = self.top_left()
+        bottom_right = self.bottom_right()
+        if ((point.x > top_left[0]) and (point.x < top_left[0] + self.width)) \
+                and ((point.y > bottom_right[1]) and (point.y < bottom_right[1] + self.height)):
+            return True
+        return False
