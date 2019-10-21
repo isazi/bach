584ac6e6ad043e3146be0af40572a1b9cce3309e
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-21 15:29:43 +0800

    Use Entity object.

--- bach/graphics.py
+++ bach/graphics.py
@@ -1,16 +1,16 @@
 import cv2
 
 
-def draw_bounding_box(img, label, color, x, y, w, h):
+def draw_bounding_box(image, entity, color):
     """
     Draw a bounding box around an object.
     """
-    top_left = (int(x - (w / 2)), int(y - (h / 2)))
-    bottom_right = (int(x + (w / 2)), int(y + (h / 2)))
-    cv2.rectangle(img, top_left, bottom_right, color, 2)
-    label_point = (int(x - (w / 2)), int(y - (h / 2)) - 10)
-    cv2.putText(img, label, label_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    return img
+    top_left = (int(entity.position.x - (entity.width / 2)), int(entity.position.y - (entity.height / 2)))
+    bottom_right = (int(entity.position.x + (entity.width / 2)), int(entity.position.y + (entity.height / 2)))
+    cv2.rectangle(image, top_left, bottom_right, color, 2)
+    label_point = (int(entity.position.x - (entity.width / 2)), int(entity.position.y - (entity.height / 2)) - 10)
+    cv2.putText(image, entity.label, label_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    return image
 
 
 def sharpen_image(image):
