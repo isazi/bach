ab5b70650bcd194f25ddd3da8f3a7eb80544c37f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-24 16:34:48 +0800

    Fixed code.

--- bach/behavior.py
+++ bach/behavior.py
@@ -6,6 +6,6 @@ def encounter(entities):
     Detect encounters between entities in a group.
     """
     for pair in itertools.combinations(entities, 2):
-        if entities[pair[0]].marker != -1 and entities[pair[1]].marker != -1:
-            if entities[pair[0]].overlap(entities[pair[1]]):
-                print("Interaction: {} <-> {}".format(pair[0], pair[1]))
+        if pair[0].marker != -1 and pair[1].marker != -1:
+            if pair[0].overlap(pair[1]):
+                print("Interaction: {} <-> {}".format(pair[0].marker, pair[1].marker))
