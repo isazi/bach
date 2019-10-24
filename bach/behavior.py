982baee2b66b0607b84d667ed296b7b2532d05ba
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-24 16:44:42 +0800

    Check both overlaps.

--- bach/behavior.py
+++ bach/behavior.py
@@ -7,5 +7,5 @@ def encounter(entities):
     """
     for pair in itertools.combinations(entities, 2):
         if pair[0].marker != -1 and pair[1].marker != -1:
-            if pair[0].overlap(pair[1]):
+            if pair[0].overlap(pair[1]) or pair[1].overlap(pair[0]):
                 print("Interaction: {} <-> {}".format(pair[0].marker, pair[1].marker))
