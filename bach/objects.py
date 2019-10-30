21ceb72b0258f48d1d090539316415c490e63dd2
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-10-30 12:54:28 +0800

    Changed how position and size are updated.

--- bach/objects.py
+++ bach/objects.py
@@ -41,14 +41,14 @@ class Entity:
         """
         Update the position of the entity.
         """
-        self.position.x = (self.position.x + point.x) / 2
-        self.position.y = (self.position.y + point.y) / 2
+        self.position.x = point.x
+        self.position.y = point.y
         self.box.update(self.position, self.width, self.height)
 
     def update_size(self, width, height):
         """
         Update the size of the entity.
         """
-        self.width = (self.width + width) / 2
-        self.height = (self.height + height) / 2
+        self.width = width
+        self.height = height
         self.box.update(self.position, self.width, self.height)
