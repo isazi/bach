20562e7991cd583b46557a289885472a3c769898
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-11-12 10:32:22 +0800

    Store encounters in some data structure.

--- bach/behavior.py
+++ bach/behavior.py
@@ -1,10 +1,10 @@
 import itertools
 
 
-def encounter(entities):
+def encounter(entities, encounters):
     """
     Detect encounters between entities in a group.
     """
     for pair in itertools.combinations(entities, 2):
         if pair[0].overlap(pair[1]):
-            print("Encounter: {} <-> {}".format(pair[0].marker, pair[1].marker))
+            encounters.append(pair)
