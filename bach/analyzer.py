0034da6d6a786addbe41167cc35cc535c1bb4704
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2021-01-06 13:42:20 +0100

    Working on distance and speed.

--- bach/analyzer.py
+++ bach/analyzer.py
@@ -14,6 +14,7 @@ def command_line():
 
 def analysis(arguments, input_file):
     entities = dict()
+    print("# frame id speed distance")
     for line in input_file:
         if line[0] == "#":
             continue
@@ -21,7 +22,9 @@ def analysis(arguments, input_file):
         frame_counter = int(items[0])
         entity_id = int(items[1])
         new_point = bach.geometry.Point(float(items[2]), float(items[3]))
+        distance = 0.0
         if entity_id in entities:
+            distance = bach.geometry.distance(new_point, entities[entity_id].position())
             entities[entity_id].update_position(new_point, float(items[4]), float(items[5]), frame_counter)
         else:
             entity = bach.entities.Entity(marker=entity_id,
@@ -30,17 +33,17 @@ def analysis(arguments, input_file):
                                           seen=frame_counter)
             entity.box = bach.geometry.Rectangle(new_point, float(items[4]), float(items[5]))
             entities[entity_id] = entity
-        # Entity's current speed
+        # Entity's current speed and distance travelled
         if entity_id != -1:
-            print("{} {} {}".format(frame_counter, entity_id, entities[entity_id].speed))
-    # Entity's average speed
+            print("{} {} {} {}".format(frame_counter, entity_id, entities[entity_id].speed, distance))
+    # Entity's average speed and total distance
+    print("# id average_speed total_distance")
     for entity_id in entities:
         if entity_id != -1:
-            print("{} {}".format(entity_id, entities[entity_id].average_speed()))
+            print("# {} {} {}".format(entity_id, entities[entity_id].average_speed(), entities[entity_id].distance))
     # Clean up
     if arguments.debug:
         print("# Entities: {}".format(len(entities)))
-    if arguments.debug:
         print()
 
 
